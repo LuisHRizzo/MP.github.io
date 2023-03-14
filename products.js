76c0e0cd82757a7864a18f84ff709485e654b6c1
Luis Rizzo <luishrizzo@gmail.com>
2023-03-14 18:25:11 -0300

    cosmetics, agregado de stilos, footer, banner.

---
+++ products.js
@@ -0,0 +1,75 @@
+const products = [
+    {
+        id: 1,
+        name: "Remera",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/remeraBlanca.jpeg",
+        image2: "./assets/images/remeraBlanca.jpeg",
+        image3: "./assets/images/remeraBlanca.jpeg"
+    },
+    {
+        id: 2,
+        name: "Cartera con Flecos",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraFlecos.jpeg",
+        image2: "./assets/images/carteraFlecos2.jpeg",
+        image3: "./assets/images/carteraFlecos2.jpeg"
+    },
+    {
+        id: 3,
+        name: "Cartera Marron",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraMarron.jpeg",
+        image2: "./assets/images/carteraMarron.jpeg",
+        image3: "./assets/images/carteraMarron.jpeg"
+    },
+    {
+        id: 4,
+        name: "Carterita Marron con flecos",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraMarronFlecos.jpeg",
+        image2: "./assets/images/carteraMarronFlecos.jpeg",
+        image3: "./assets/images/carteraMarronFlecos.jpeg"
+    },
+    {
+        id: 5,
+        name: "Cartera Negra",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraNegra.jpeg",
+        image2: "./assets/images/carteritaNegra1.jpeg",
+        image3: "./assets/images/carteritaNegra2.jpeg"
+    },
+    {
+        id: 6,
+        name: "Mochila de Cuero",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/mochilitaFemGris.jpeg",
+        image2: "./assets/images/mochilitaFemGris.jpeg",
+        image3: "./assets/images/mochilitaFemGris.jpeg"
+    },
+    {
+        id: 7,
+        name: "Campera de Cuero",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/camperaDeCuero.jpeg",
+        image2: "./assets/images/camperaDeCuero.jpeg",
+        image3: "./assets/images/camperaDeCuero.jpeg"
+    },
+    {
+        id: 8,
+        name: "Cartera Verde",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraVerde.jpeg",
+        image2: "./assets/images/carteraVerde.jpeg",
+        image3: "./assets/images/carteraVerde.jpeg"
+    }
+];
+
--- script.js
+++ script.js
@@ -2,83 +2,84 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Array de objetos con los productos
-    const products = [
-        {
-            id: 1,
-            name: "Remera",
-            category: "Ropa",
-            description: "lorem   Ipsum",
-            image: "./assets/images/remeraBlanca.jpeg",
-            image2: "./assets/images/remeraBlanca.jpeg",
-            image3: "./assets/images/remeraBlanca.jpeg"
-        },
-        {
-            id: 2,
-            name: "Cartera con Flecos",
-            category: "Carteras",
-            description: "lorem   Ipsum",
-            image: "./assets/images/carteraFlecos.jpeg",
-            image2: "./assets/images/carteraFlecos2.jpeg",
-            image3: "./assets/images/carteraFlecos2.jpeg"
-        },
-        {
-            id: 3,
-            name: "Cartera Marron",
-            category: "Carteras",
-            description: "lorem   Ipsum",
-            image: "./assets/images/carteraMarron.jpeg",
-            image2: "./assets/images/carteraMarron.jpeg",
-            image3: "./assets/images/carteraMarron.jpeg"
-        },
-        {
-            id: 4,
-            name: "Carterita Marron con flecos",
-            category: "Carteras",
-            description: "lorem   Ipsum",
-            image: "./assets/images/carteraMarronFlecos.jpeg",
-            image2: "./assets/images/carteraMarronFlecos.jpeg",
-            image3: "./assets/images/carteraMarronFlecos.jpeg"
-        },
-        {
-            id: 5,
-            name: "Cartera Negra",
-            category: "Carteras",
-            description: "lorem   Ipsum",
-            image: "./assets/images/carteraNegra.jpeg",
-            image2: "./assets/images/carteritaNegra1.jpeg",
-            image3: "./assets/images/carteritaNegra2.jpeg"
-        },
-        {
-            id: 6,
-            name: "Mochila de Cuero",
-            category: "Ropa",
-            description: "lorem   Ipsum",
-            image: "./assets/images/mochilitaFemGris.jpeg",
-            image2: "./assets/images/mochilitaFemGris.jpeg",
-            image3: "./assets/images/mochilitaFemGris.jpeg"
-        },
-        {
-            id: 7,
-            name: "Campera de Cuero",
-            category: "Ropa",
-            description: "lorem   Ipsum",
-            image: "./assets/images/camperaDeCuero.jpeg",
-            image2: "./assets/images/camperaDeCuero.jpeg",
-            image3: "./assets/images/camperaDeCuero.jpeg"
-        },
-        {
-            id: 8,
-            name: "Cartera Verde",
-            category: "Carteras",
-            description: "lorem   Ipsum",
-            image: "./assets/images/carteraVerde.jpeg",
-            image2: "./assets/images/carteraVerde.jpeg",
-            image3: "./assets/images/carteraVerde.jpeg"
-        }
-    ];
+  // Array de objetos con los productos
 
-    
+    const products = [
+    {
+        id: 1,
+        name: "Remera",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/remeraBlanca.jpeg",
+        image2: "./assets/images/remeraBlanca.jpeg",
+        image3: "./assets/images/remeraBlanca.jpeg",
+    },
+    {
+        id: 2,
+        name: "Cartera con Flecos",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraFlecos.jpeg",
+        image2: "./assets/images/carteraFlecos2.jpeg",
+        image3: "./assets/images/carteraFlecos2.jpeg"
+    },
+    {
+        id: 3,
+        name: "Cartera Marron",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraMarron.jpeg",
+        image2: "./assets/images/carteraMarron.jpeg",
+        image3: "./assets/images/carteraMarron.jpeg"
+    },
+    {
+        id: 4,
+        name: "Carterita Marron con flecos",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraMarronFlecos.jpeg",
+        image2: "./assets/images/carteraMarronFlecos.jpeg",
+        image3: "./assets/images/carteraMarronFlecos.jpeg"
+    },
+    {
+        id: 5,
+        name: "Cartera Negra",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraNegra.jpeg",
+        image2: "./assets/images/carteritaNegra1.jpeg",
+        image3: "./assets/images/carteritaNegra2.jpeg"
+    },
+    {
+        id: 6,
+        name: "Mochila de Cuero",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/mochilitaFemGris.jpeg",
+        image2: "./assets/images/mochilitaFemGris.jpeg",
+        image3: "./assets/images/mochilitaFemGris.jpeg"
+    },
+    {
+        id: 7,
+        name: "Campera de Cuero",
+        category: "Ropa",
+        description: "lorem   Ipsum",
+        image: "./assets/images/camperaDeCuero.jpeg",
+        image2: "./assets/images/camperaDeCuero.jpeg",
+        image3: "./assets/images/camperaDeCuero.jpeg"
+    },
+    {
+        id: 8,
+        name: "Cartera Verde",
+        category: "Carteras",
+        description: "lorem   Ipsum",
+        image: "./assets/images/carteraVerde.jpeg",
+        image2: "./assets/images/carteraVerde.jpeg",
+        image3: "./assets/images/carteraVerde.jpeg"
+    }
+];
+
+
 
     // Función para mostrar los productos en la página
     function showProducts(products) {
@@ -94,17 +95,19 @@ document.addEventListener("DOMContentLoaded", () => {
               <div class="card mb-4 shadow-sm">
                 <div id="carousel-${product.id}" class="carousel slide" data-bs-ride="carousel">
                   <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img src="${product.image}" class="d-block w-100" alt="${product.name}">
+                    <div class="carousel-item active ">
+                    <figure class="rounded mb-6">
+                      <img src="${product.image}" class="card-img-top d-block w-100" alt="${product.name}">
+                    </figure>
                     </div>
                     <div class="carousel-item">
-                      <img src="${product.image2}" class="d-block w-100" alt="${product.name}">
+                      <img src="${product.image2}" class="card-img-top d-block w-100" alt="${product.name}">
                     </div>
                     <div class="carousel-item">
-                      <img src="${product.image3}" class="d-block w-100" alt="${product.name}">
+                      <img src="${product.image3}" class="card-img-top d-block w-100" alt="${product.name}">
                     </div>
                   </div>
->
+
                 </div>
                 <div class="card-body">
                   <h5 class="card-title">${product.name}</h5>
